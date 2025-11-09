dbf9b7b7f1462bb9c4a11b79860e5d5df38c97e3
Kevin Bourgeix <kevin.bourgeix@gmail.com>
2025-11-09 17:04:15 +0100

    🐛 fix autosave issue with Harpoon

--- lua/custom/plugins/autosave.lua
+++ lua/custom/plugins/autosave.lua
@@ -2,6 +2,19 @@ return {
   'okuuva/auto-save.nvim',
   version = '^1.0.0',
   cmd = 'ASToggle', -- optionnel, lazy loading à la commande
-  event = { 'InsertLeave', 'TextChanged' }, -- lazy loading à ces événements
   opts = {}, -- configuration par défaut, ou personnalisez ici
+      -- function that takes the buffer handle and determines whether to save the current buffer or not
+      -- return true: if buffer is ok to be saved
+      -- return false: if it's not ok to be saved
+      -- if set to `nil` then no specific condition is applied
+      condition = function(buf)
+        -- Disable auto-save for the harpoon plugin, otherwise it just opens and closes
+        -- https://github.com/ThePrimeagen/harpoon/issues/434
+        local filetype = vim.bo[buf].filetype
+        if filetype == "harpoon" then
+          return false
+        end
+
+        return true
+      end,
 }
